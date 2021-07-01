3c41dac48edd3e0812661038e48b268da331f04f
Polina Shneider <beatrixlinkoln@gmail.com>
2021-07-01 18:13:10 +0300

    Build version

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@ import Header from "./components/Header/Header";
 import PhotoList from "./components/PhotoList/PhotoList";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <PhotoList/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <PhotoList/>
+        </div>
+    );
 }
+
+(function () {
+    const script = document.createElement('script');
+    script.async = true;
+    script.innerHTML = `(function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+          'gtm.start':
+              new Date().getTime(), event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-TF5QKDQ');`;
+    document.head.append(script);
+
+    const bodyScript = document.createElement('noscript');
+    bodyScript.async = true;
+    const iframe = document.createElement('iframe');
+    iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-TF5QKDQ";
+    iframe.height = 0;
+    iframe.width = 0;
+    iframe.style = "display:none;visibility:hidden";
+    bodyScript.append(iframe);
+    document.body.prepend(bodyScript);
+})();
 
 export default App;
